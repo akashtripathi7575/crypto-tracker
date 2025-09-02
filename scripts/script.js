42f83a98d14c39a2dad1a305189749dadafc302a
Akash Tripathi <aakashtripathi54@gmail.com>
2025-09-02 12:32:02 +0530

    Successfully fetch data from API

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,28 @@
-const resultSpan = document.querySelector('#conversion-result');
-resultSpan.textContent = '83.40 USD';
+import apiKey from "./config.js";
+
+const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=solana`;
+
+async function fetchCryptoData() {
+    try {
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "x-cg-demo-api-key": apiKey,
+            "Accept": "application/json"
+        }
+    });
+   
+   const data = await response.json();
+   console.log("Crypto data fetched successfully:", data);
+
+
+    } catch (error) {
+        console.error("Error fetching crypto data:", error);
+    }
+}
+
+    fetchCryptoData();
+
+// const resultSpan = document.querySelector
+// ('#conversion-result');
+// resultSpan.textContent = '83.40 USD';
