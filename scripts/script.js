20c4e6aa470c01595c84a04a1948b77f9f82290b
Akash Tripathi <aakashtripathi54@gmail.com>
2025-09-03 16:20:50 +0530

    feat: add event listener for convert button to fetch and display crypto data

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,18 @@
 import apiKey from "./config.js";
 
-const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=solana`;
+const amountInput = document.querySelector('#amount-input');
+const resultSpan =  document.querySelector('#conversion-result');
+const coinInput = document.querySelector('#coin-input');
+const convertBtn = document.querySelector('#convert-btn');
+const coinImage = document.querySelector('#coinImage');
+const coinName = document.querySelector('#coinName');
+const coinPrice = document.querySelector('#coinPrice');
 
-async function fetchCryptoData() {
+
+async function fetchCryptoData(coinId, amount) {
+    
+    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinId}`;
+
     try {
     const response = await fetch(url, {
         method: "GET",
@@ -13,16 +23,44 @@ async function fetchCryptoData() {
     });
    
    const data = await response.json();
-   console.log("Crypto data fetched successfully:", data);
 
+   if(!data || data.length === 0) {
+       resultSpan.textContent = "Coint not found";
+       coinImage.src = "";
+       coinName.textContent = "";
+       coinPrice.textContent = "";
+       return;
+   }
+
+   const coin = data[0];
+
+   coinName.textContent = coin.name;
+   coinPrice.textContent = `Coin price is = $${coin.current_price}`;
+   coinImage.src = coin.image;
+
+   const totalValue = amount * coin.current_price;
+   const value = resultSpan.textContent = `${amount} ${coin.symbol.toUpperCase()} = $${totalValue.toFixed(3)}`;
+   console.log(value);
 
     } catch (error) {
         console.error("Error fetching crypto data:", error);
+        resultSpan.textContent = "Error fetching data. Try again later";
     }
 }
 
-    fetchCryptoData();
+convertBtn.addEventListener('click', () => {
+    const amount = parseFloat(amountInput.value);
+    const coinId = coinInput.value.trim().toLowerCase();
 
-// const resultSpan = document.querySelector
-// ('#conversion-result');
-// resultSpan.textContent = '83.40 USD';
+    if(!coinId || isNaN(amount) || amount <= 0) {
+        resultSpan.textContent = "Enter a valid coin or amount.";
+        coinImage.src = "";
+        coinName.textContent = "";
+        coinPrice.textContent = "";
+        return;
+    }
+
+    fetchCryptoData(coinId, amount);
+});
+
+
